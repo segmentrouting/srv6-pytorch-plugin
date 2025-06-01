00aea0a4967e372556c735019e39e6a3a9976927
brmcdoug <brmcdoug@cisco.com>
2025-06-01 11:33:34 -0700

    dups

--- srv6_plugin.py
+++ srv6_plugin.py
@@ -17,7 +17,6 @@ class DemoPlugin:
     
     def init_process_group(self, backend='gloo', **kwargs):
         """Initialize distributed training and program routes"""
-        #logger.info("Initializing distributed training...")
         # First, initialize PyTorch distributed
         if not init_distributed():
             logger.error("Failed to initialize distributed training")
@@ -25,14 +24,10 @@ class DemoPlugin:
         
         try:
             # Get information about all nodes
-            logger.info(" Getting node information...")
+            logger.info("Getting node information...")
             nodes = get_all_nodes()
             
-            # Program routes
-            #logger.info("  Begin programming routes...")
-            self.network_programmer.program_all_routes(nodes)
-            
-            logger.info(" Initialization completed successfully")
+            logger.info("Initialization completed successfully")
             return True
             
         except Exception as e:
